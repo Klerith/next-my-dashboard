3eb6ba02b8c2f4c896b31409ef4706bfbc6579dd
Fernando Herrera <fernando.herrera85@gmail.com>
2023-06-12 09:58:10 -0400

    shopping-cart tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pokemons/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/shopping-cart/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
